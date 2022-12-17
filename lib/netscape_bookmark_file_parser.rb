eb580aa09dce806c2496bcc7547d56c3103566e8
Sapeu <sapeu.lee@foxmail.com>
2022-12-17 15:29:42 +0800

    feat: init netscape_bookmark_file_parser

---
+++ lib/netscape_bookmark_file_parser.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "netscape_bookmark_file_parser/version"
+
+module NetscapeBookmarkFileParser
+  class Error < StandardError; end
+  # Your code goes here...
+end
